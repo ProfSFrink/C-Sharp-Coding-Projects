f2152cfd130611675ace2f7f1ef82455e7170ee8
Steven Partlow <profsfrink@gmail.com>
2023-02-26 20:55:28 +0000

    COMPLETE MVC APP Assignment Part 9:
    StudentMVC
    
    Instructor.cs
    - ADD Id property
    - ADD FirstName property
    - ADD LastName property

---
+++ StudentMVC/StudentMVC/Models/Instructor.cs
@@ -0,0 +1,26 @@
+﻿// Advanced Software and Web Developer Diploma
+// Part X: C# and .NET Framework - Part 2
+// by Pitman Training / The Tech Academy
+
+// MODULE 11: Creating MVC Applications
+// AUTHOR: Steven Partlow
+// DATE: 26/02/2023
+
+// INSTRUCTOR CLASS DEFINITION
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentMVC.Models
+{
+
+    public class Instructor
+    {
+        public int Id { get; set; } // Define a Id property as a integer data type
+        public string FirstName { get; set; } // Define a FirstName property as a string data type  
+        public string LastName { get; set; } // Define a LastName property as a string data type
+    } // END Instructor CLASS
+
+} // END StudentMVC.Models
