ede912f30d49acfbda3f8b3c95371699ed9de7be
Steven Partlow <profsfrink@gmail.com>
2023-02-26 20:29:25 +0000

    COMPLETE  MVC App Assignment Part 8
    StudentMVC
    - ADD Instructor.cshtml view
    
    HomeController.cs
    - ADD Instructor Action with one parameter

--- StudentMVC/StudentMVC/Controllers/HomeController.cs
+++ StudentMVC/StudentMVC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@
 
 // MODULE 11: Creating MVC Applications
 // AUTHOR: Steven Partlow
-// DATE: 25/02/2023
+// DATE: 26/02/2023
 
 using System;
 using System.Collections.Generic;
@@ -19,25 +19,32 @@ namespace StudentMVC.Controllers
         public ActionResult Index()
         {
             return View();
-        }
+        } // END Index
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
-        }
+        } // END About
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Contact Page - Academy of Learning";
 
             return View();
-        }
+        } // END Contact
 
+        public ActionResult Instructor(int id) // Take parameter taken in from browser URL and store value as integer id
+        {
+            ViewBag.Id = id; // Add value of id to ViewBag dictionary
+
+            return View(); // Return Instructor view
+        } // END Instructor
+         
         public ActionResult Instructors()
         {
             return View();
-        }
+        } // END Instructors
     }
 }
