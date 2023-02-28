df18265d2462782d2a0d643b0644b0a9f0bb91e4
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-02-28 14:24:48 +0000

    COMPLETED MVC Sample Application Part 5:
    NewsletterAppMVC
    
    HomeController.cs
    - UPDATE SignUp method to check if all three input are empty if they are return error view else return success view
    
    readme.md
    - UPDATE With tutorial description

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -16,9 +16,17 @@ namespace NewsletterAppMVC.Controllers
         [HttpPost] // The below method is a post
 
         // Take the three inputs from the Sign Up web form and map them into each input parameter
-        public ActionResult SignUp(string FirstName, string LastName, string EmailAddress)
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
         {
-            return null;
+            // Check if any of the three input parameters are null or empty then if
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml"); // they are return view error.cshtml
+            } // End IF
+            else
+            {
+                return View("Success"); // If all inputs are valid return view Success.cshtml
+            } // End ELSE
         } // END SignUp METHOD
 
         public ActionResult About()
