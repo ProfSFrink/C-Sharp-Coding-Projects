751febb5edb43acb51f802c8a1ee5b7518ee683a
Steven Partlow <profsfrink@gmail.com>
2023-03-05 21:00:54 +0000

    COMPLETE Entity Framework Tutorial Part 3:
    NewsletterAppMVC
    
    HomeController.cs:
    - REMOVE ConnectionString variable as now obsolete
    - UPDATE Admin() delete out all old ADO.NET database calls and replace them with entity framework database calls
    - UPDATE SignUp() delete out all old ADO.NET database calls and replace them with entity framework database calls

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -21,9 +21,6 @@ namespace NewsletterAppMVC.Controllers
 
     public class HomeController : Controller
     {
-        // Our connection string so we can connect to our database
-        private readonly string connectionString = @"Data Source=PROFSFRINK-PC\SQLEXPRESS;Initial Catalog=db_Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
         public ActionResult Index()
         {
             return View();
@@ -41,29 +38,19 @@ namespace NewsletterAppMVC.Controllers
             } // End IF
             else
             {
-                // A SQL query store as a string will add the three method input parameters to the matching columns in our SignUps table in our Newsletter database
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                                        (@FirstName, @LastName, @EmailAddress)";
+                /* ENTITY FRAMEWORK DATABASE CALLS */
 
-                // OPEN a new SqlConnection called connection using our connectionString
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                // Create new connection to the newsletter database called db
+                using (db_NewsletterEntities db = new db_NewsletterEntities())
                 {
-                    // Create a new SQL command object using the connection created and the queryString defined above
-                    SqlCommand command = new SqlCommand(queryString, connection);
+                    var signup = new SignUp(); // Create a new instance of the SignUp class called signup
+                    signup.FirstName = firstName; // Assign the passed in parameter firstName to signup.FirstName
+                    signup.LastName = lastName; // Assign the passed in parameter lastName to signup.LastName
+                    signup.EmailAddress = emailAddress; // Assign the passed in parameter emailAddress to signup.EmailAddress
 
-                    // Define the parameters used within our queryString
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar); // Define this parameter as a SQL VarChar data type
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar); // Define this parameter as a SQL VarChar data type
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar); // Define this parameter as a SQL VarChar data type
-
-                    command.Parameters["@FirstName"].Value = firstName; // Set SQL Query parameter FirstName equal to the value of C# variable firstName
-                    command.Parameters["@LastName"].Value = lastName; // Set SQL Query parameter LastName equal to the value of C# variable lastName
-                    command.Parameters["@EmailAddress"].Value = emailAddress; // Set SQL Query parameter EmailAddress equal to the value of C# variable emailAddress
-
-                    connection.Open(); // Open the SQL connection to the db_Newsletter database
-                    command.ExecuteNonQuery(); // Execute the SQL query defined in command
-                    connection.Close(); // Close the SQL connection to the db_Newsletter database
-                } // CLOSE SqlConnection
+                    db.SignUps.Add(signup); // Add the signup instance to the SignUps table in our database
+                    db.SaveChanges(); // Save the updated database
+                } // End USING
 
                 return View("Success"); // If all inputs are valid return view Success.cshtml
             } // End ELSE
@@ -72,43 +59,26 @@ namespace NewsletterAppMVC.Controllers
 
         public ActionResult Admin()
         {
-            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber FROM SignUps"; // This queryString will select all records within the database
+            /* ENTITY FRAMEWORK DATABASE CALLS */
 
-            List<NewsletterSignUp> signups = new List<NewsletterSignUp>(); // An empty list containing instances of our NewsletterSignUp model (class)
-
-            using (SqlConnection connection = new SqlConnection(connectionString)) // Open a new SQL connectio using our connectionString
+            // Create new connection to the newsletter database called db
+            using (db_NewsletterEntities db = new db_NewsletterEntities()) 
             {
-                SqlCommand command = new SqlCommand(queryString, connection); // Create a new SqlCommand object using queryString and connection
-
-                connection.Open(); // Open the SQL connection
-
-                SqlDataReader reader = command.ExecuteReader(); // Create a new SqlDataReader object and store the data retrieved by our SQL command within it 
-
-                while (reader.Read()) // Keep iterating until there are no more records to read
+                var signups = db.SignUps; // Take all the records from the db.SignUps table and assign them the list signups (This is a list of SignUp.cs class instances)
+                
+                var signupVms = new List<SignupVm>(); // Create list of SignupVm view model objects
+                foreach (var signup in signups) // Take the signups list and iterate through it assigning each instance to a local variable signup
                 {
-                    var signup = new NewsletterSignUp(); // Create a new instance of the NewsLetterSignUp model and call it signup
+                    var signupVm = new SignupVm(); // Create a local instance of the signupVm view model
+                    signupVm.FirstName = signup.FirstName; // Map the current instance of signup.FirstName to signupVm.FirstName
+                    signupVm.LastName = signup.LastName; // Map the current instance of signup.LastName to signupVm.LastName
+                    signupVm.EmailAddress = signup.EmailAddress; // Map the current instance of signup.EmailAddress to signupVm.EmailAddress
+                    signupVms.Add(signupVm); // Take the local instance signupVm and add it to our main signupVms list
+                } // End FOREACH
 
-                    signup.Id = Convert.ToInt32(reader["Id"]); // Take the current records Id column and assign it to the signup.Id property after casting it to a integer
-                    signup.FirstName = reader["FirstName"].ToString(); // Take the current records FirstName column and assign ut to the signup.FirstName property after casting it to a string
-                    signup.LastName = reader["LastName"].ToString(); // Take the current records LastName column and assign it to the signup.LastName property after casting it to a string
-                    signup.EmailAddress = reader["EmailAddress"].ToString(); // Take the current records EmailAddress column and assign it to the signup.EmailAddress property after casting it to a string
-                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString(); // Take the current records SocialSecurityNumber column and assign it to the signup.SocialSecurityNumber property after casting it to a string
-
-                    signups.Add(signup); // Add the signup instance of the NewsletterSignUp model to the signups list
-                } // End WHILE
+                return View(signupVms); // Pass the signupVms to the view
             } // End USING
 
-            var signupVms = new List<SignupVm>(); // Create list of SignupVm view model objects
-            foreach (var signup in signups) // Take the signups list and iterate through it assigning each instance to a local variable signup
-            {
-                var signupVm = new SignupVm(); // Create a local instance of the signupVm view model
-                signupVm.FirstName = signup.FirstName; // Map the current instance of signup.FirstName to signupVm.FirstName
-                signupVm.LastName = signup.LastName; // Map the current instance of signup.LastName to signupVm.LastName
-                signupVm.EmailAddress = signup.EmailAddress; // Map the current instance of signup.EmailAddress to signupVm.EmailAddress
-                signupVms.Add(signupVm); // Take the local instance signupVm and add it to our main signupVms list
-            } // End FOREACH
-
-            return View(signupVms); // Pass the signupVms to the view
         } // End Admin METHOD
 
     }
