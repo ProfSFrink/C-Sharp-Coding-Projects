87a5164dbf04d6b4fbeef282a18ba50ccc52b2a9
Steven Partlow <profsfrink@gmail.com>
2023-03-07 18:09:55 +0000

    COMPLETE Entity Framework Tutoria Part 6:
    NewsletterAppMVC:
    - UPDATE master branch

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -28,7 +28,16 @@ namespace NewsletterAppMVC.Controllers
             // Create new connection to the newsletter database called db
             using (db_NewsletterEntities db = new db_NewsletterEntities())
             {
-                var signups = db.SignUps; // Take all the records from the db.SignUps table and assign them the list signups (This is a list of SignUp.cs class instances)
+                /* SELECT ALL NON-REMOVED USERS USING LAMBDA EXPRESSIONS */
+
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList(); // Use a lambda expressions to select all rows from the SignUps table where the Removed property is set to null, then add all relevant records and assign them to a list and assign it the signups variable
+
+                /* SELECT ALL NON-REMOVED USERS USING LINQ EXPRESSIONS */
+
+                /* Select any row from the SignUps table, where c represents the current row being check, where the Removed column of that row is equal to null then assign all those row to a list and assign that list to the signups variable */
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
 
                 var signupVms = new List<SignupVm>(); // Create list of SignupVm view model objects
                 foreach (var signup in signups) // Take the signups list and iterate through it assigning each instance to a local variable signup
@@ -45,6 +54,23 @@ namespace NewsletterAppMVC.Controllers
             } // End USING
 
         } // End of Index() Method
+
+        /* Unsubscribe method */
+
+        public ActionResult Unsubscribe(int Id) // Define Unsubscribe method with one input integer parameter Id
+        {
+
+            using (db_NewsletterEntities db = new db_NewsletterEntities()) // Open a connection to our Newsletter database
+            {
+                var signup = db.SignUps.Find(Id); // Find the row in our SignUps table with the matching Id provided by input parameter Id
+
+                signup.Removed = DateTime.Now; // Assign the current date and time to the Removed column of the signup row
+                db.SaveChanges(); // Save the update row to the database
+            }
+
+            return RedirectToAction("Index"); // Return the Admin index view
+
+        } // End Unsubscribe Method
 
     }  // End CLASS AdminController
 
